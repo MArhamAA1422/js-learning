dd5ca68604dddb6cbae83f6f203a44c5a5e5776d
Md::Arham <arham511624@gmail.com>
2025-07-30 17:38:38 +0600

    Basic ToDO LIST: my solution

---
+++ scripts/11-todo-list.js
@@ -0,0 +1,21 @@
+const todoList = [];
+function addTodo() {
+    const inputElement = document.querySelector('.js-todo-name');
+    if (inputElement.value.length) todoList.push(inputElement.value);
+    
+    inputElement.value = '';
+    // console.log(todoList);
+
+    showTodoList();
+}
+
+function showTodoList() {
+    const todoListElement = document.querySelector('.js-todo-list');
+    let allList = '';
+
+    for (let i = 0; i < todoList.length; i++) {
+        allList += `<p>${todoList[i]}</p>`;
+    }
+
+    todoListElement.innerHTML = allList;
+}
