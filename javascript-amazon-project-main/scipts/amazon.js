69615761545cb8ed5909885ca048f8aaa09406d2
Md::Arham <arham511624@gmail.com>
2025-07-31 23:58:12 +0600

    Added message show

--- javascript-amazon-project-main/scipts/amazon.js
+++ javascript-amazon-project-main/scipts/amazon.js
@@ -42,7 +42,7 @@ products.forEach(product => {
 
             <div class="product-spacer"></div>
 
-            <div class="added-to-cart js-added-to-cart">
+            <div class="added-to-cart js-added-to-cart-${product.id}">
                 <img src="images/icons/checkmark.png">
                 Added
             </div>
@@ -85,5 +85,7 @@ document.querySelectorAll('.js-add-to-cart').forEach(button => {
         });
 
         document.querySelector('.js-cart-quantity').innerHTML = `${cartQuantity}`;
+
+        document.querySelector(`.js-added-to-cart-${productId}`).classList.add('show-added-to-cart');
     });
 });
