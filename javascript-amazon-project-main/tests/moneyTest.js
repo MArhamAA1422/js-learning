d7c3911ceaf30a62c214d51a1c9a09d11a5598a8
Md::Arham <arham511624@gmail.com>
2025-08-02 21:49:27 +0600

    Create automated tests for formatCurrency

--- javascript-amazon-project-main/tests/moneyTest.js
+++ javascript-amazon-project-main/tests/moneyTest.js
@@ -1,6 +1,25 @@
 import { formatCurrency } from "../scipts/utils/money.js";
 
+console.log('test suite: formatCurrency');
+
+/* Basic Test Case */
+console.log('converts cents into dollars');  // TC name
 if (formatCurrency(2095) === '20.95') {
+    console.log('passed');
+} else {
+    console.log('failed');
+}
+
+console.log('works with 0');
+if (formatCurrency(0) === '0.00') {
+    console.log('passed');
+} else {
+    console.log('failed');
+}
+
+/* Edge Test Case */
+console.log('rounds up to the nearest cent')
+if (formatCurrency(2000.5) === '20.01') {
     console.log('passed');
 } else {
     console.log('failed');
