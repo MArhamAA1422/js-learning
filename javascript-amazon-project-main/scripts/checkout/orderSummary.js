35fdddebcf7e3cda1c17488ed70ca4894cb22d93
Md::Arham <arham511624@gmail.com>
2025-08-04 13:41:01 +0600

    Fix issues in test, dummy class in div, delete button test

--- javascript-amazon-project-main/scripts/checkout/orderSummary.js
+++ javascript-amazon-project-main/scripts/checkout/orderSummary.js
@@ -9,7 +9,7 @@ export function renderOrderSummary() {
 
     let cartSummary = '';
 
-    // renderCheckoutHeader();
+    renderCheckoutHeader();
 
     cart.forEach(cartItem => {
         const productId = cartItem.productId;
@@ -42,7 +42,7 @@ export function renderOrderSummary() {
                     </span>
                     <input class="quantity-input js-quantity-input js-quantity-input-${matchingProduct.id}" data-product-id="${matchingProduct.id}">
                     <span class="save-quantity-link link-primary js-save-quantity" data-product-id="${matchingProduct.id}">Save</span>
-                    <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
+                    <span class="delete-quantity-link link-primary js-delete-link js-delete-link-${matchingProduct.id}" data-product-id="${matchingProduct.id}">
                     Delete
                     </span>
                 </div>
